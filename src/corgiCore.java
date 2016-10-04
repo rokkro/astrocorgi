1c32dca76dd7da20a22f40dd2c486d5f19537746
rukkro <darrenew@gmail.com>
2016-10-03 19:31:08 -0500

    Update corgiCore.java

--- src/corgiCore.java
+++ src/corgiCore.java
@@ -2,19 +2,18 @@ import java.awt.event.KeyEvent;
 import static java.awt.event.KeyEvent.*;
 
 public  class corgiCore {
-
     //Note: Java interprets /src/img/ the same regardless of OS. Using \\src\\img\\ for windows introduces weird bugs. Windows and linux also render the fonts differently.
     public static String osPath = System.getProperty("user.dir") + "/src/img/";
-    public static int diff = 1;
-    public static int corgiState; //mental health of your corg, changes on wins/losses. 0 is neutral.
-    public static double corgiPosX = .1;
+    public static int diff = 1; //difficulty
+    public static int corgiState; //mental health of your corg, changes on wins/losses. 0 is neutral. Alters what corgi looks like.
+    public static double corgiPosX = .1; //dogposition
     public static double corgiPosY = .1;
-    public static String dogs;
-    public static String dogf;
-    private static char facing = 'E';
-    public static int health = 1;
-    public static int[] shipHP = {1,1,0,0}; //N,S,E,W
-    public static int points = 10;
+    public static String dogs;  //dog image path
+    public static String dogf;  //flipped dog image path
+    private static char facing = 'E'; //used for making corgi face last direction pressed
+    public static int health = 10; //dogHP
+    public static int[] shipHP = {2,2,2,2}; //N,S,E,W
+    public static int points = 0;
 
     public static void control(double m, boolean shield){
         corgiBall[] c = new corgiBall[1];
@@ -26,20 +25,21 @@ public  class corgiCore {
             facing = 'E';
             if(corgiPosX<=1)
                 corgiPosX+=m;
-            if(StdDraw.isKeyPressed(VK_SHIFT)) //hold shift + direction to force corgi movement
+            if(StdDraw.isKeyPressed(VK_SHIFT)) //hold shift + direction to force corgi movement. kind of a cheat/debug thing
                 corgiPosX+=.001;
-            if(shield) {
-                hitBox(s, facing);
+            if(shield) { //if corgi shield is activated
+                hitBox(s, facing); //box collision with direction corgi is facing
                 StdDraw.picture(corgiPosX + .1, corgiPosY, osPath + "shield.png");
                 if(points >= 10 && StdDraw.isKeyPressed(VK_SPACE)){ //Firing the corgicannon with 10/10 points
-                    StdDraw.picture(corgiPosX,corgiPosY, osPath + "dogsmile.png");
-                    shipHP[2]=0;
-                    for(double i=corgiPosX+.1;i<1;i+=.01)
+                    StdDraw.picture(corgiPosX,corgiPosY, osPath + "dogsmile.png"); //Bark!
+                    if(shipHP[2]>0)//reduce shipHP that was hit, if it's still alive
+                        shipHP[2]--;
+                    for(double i=corgiPosX+.1;i<1;i+=.01) //shows cannon animation
                     {
                         StdDraw.setPenColor(StdDraw.CYAN);
                         StdDraw.filledCircle(i+.02, corgiPosY,.06);
                         corgiScene.show(30);
-                        resetBalls(s);
+                        resetBalls(s); //removes balls on screen
                     }
                 }
             }
@@ -54,7 +54,8 @@ public  class corgiCore {
                 StdDraw.picture(corgiPosX - .1, corgiPosY, osPath + "shield.png", 180);
                 if(points >= 10 && StdDraw.isKeyPressed(VK_SPACE)){
                     StdDraw.picture(corgiPosX,corgiPosY, osPath + corgiImage.flip("dogsmile.png","dogsmilef.png"));
-                    shipHP[3]=0;
+                    if(shipHP[3]>0)
+                        shipHP[3]--;
                     for(double i=corgiPosX-.1;i>0;i-=.01)
                     {
                         StdDraw.setPenColor(StdDraw.CYAN);
@@ -75,7 +76,8 @@ public  class corgiCore {
                 StdDraw.picture(corgiPosX, corgiPosY + .1, osPath + "shield.png", 90);
                 if(points >= 10 && StdDraw.isKeyPressed(VK_SPACE)){
                     StdDraw.picture(corgiPosX,corgiPosY, osPath + "dogsmile.png",90);
-                    shipHP[0]=0;
+                    if(shipHP[0]>0)
+                    shipHP[0]--;
                     for(double i=corgiPosY+.1;i<1;i+=.01)
                     {
                         StdDraw.setPenColor(StdDraw.CYAN);
@@ -96,7 +98,8 @@ public  class corgiCore {
                 StdDraw.picture(corgiPosX, corgiPosY - .1, osPath + "shield.png", 270);
                 if(points >= 10 && StdDraw.isKeyPressed(VK_SPACE)){
                     StdDraw.picture(corgiPosX,corgiPosY, osPath + corgiImage.flip("dogsmile.png","dogsmilef.png"),90);
-                    shipHP[1]=0;
+                    if(shipHP[1]>0)
+                        shipHP[1]--;
                     for(double i=corgiPosY-.1;i>0;i-=.01)
                     {
                         StdDraw.setPenColor(StdDraw.CYAN);
@@ -107,7 +110,7 @@ public  class corgiCore {
                 }
             }
         }
-        switch (facing) {
+        switch (facing) { //Shows corgi facing whatever direction last pressed when not holding keyboard direction
             case 'E':
                 StdDraw.picture(corgiPosX, corgiPosY, osPath + dogs);
                 break;
@@ -121,22 +124,12 @@ public  class corgiCore {
                 StdDraw.picture(corgiPosX, corgiPosY, osPath + dogf);
                 break;
         }
-        if(shield)
+        if(shield) //hitbox if no direction is pressed
             hitBox(s, '1');
         corgiScene.show(5);
         corgiScene.reDraw();
-        if(StdDraw.isKeyPressed(VK_1))
-            for(int i=0;i<s.length;i++) {
-                s[i].mX += .00001;
-                s[i].mY += .00001;
-            }
-        if(StdDraw.isKeyPressed(VK_2))
-            for(int i=0;i<s.length;i++) {
-                s[i].mX -= .00001;
-                s[i].mY -= .00001;
-            }
     }
-    public static void setCorgi(){
+    public static void setCorgi(){ //used to get an appropriate corgi depending on how it feels from the last game.
         if(corgiState <=-1) {
             dogs = "dogsad.png";
             dogf = corgiImage.flip("dogsad.png", "dogsadf.png");
@@ -162,16 +155,16 @@ public  class corgiCore {
             corgiScene.moodText = "Your corgi is now addicted to dog treats. It's almost kind of disgusting.";
         }
     }
-    private static void hitBox(corgiBall[] s, char c){
-        for(int i=0;i<s.length;i++) {
-            if (s[i].dir == c || s[i].dir2 == c) {
+    private static void hitBox(corgiBall[] s, char c){ //that middle box
+        for(int i=0;i<s.length;i++) { //looks at every ball
+            if (s[i].dir == c || s[i].dir2 == c) { //checks if key press and ball direction match
                 corgiScene.boxColor = StdDraw.GREEN;
-                corgiScene.box();
+                corgiScene.box(); 
                 points++;
-                corgiScene.show(500);
+                corgiScene.show(500); //briefly pauses the game to show the box is green
                 s[i].reset();
             }
-            else if(s[i].dir != '0' || s[i].dir2 != '0'){ //if ball is N,E,S,W and doesn't get blocked by dog
+            else if(s[i].dir != '0' || s[i].dir2 != '0'){ //if the ball doesn't get stopped by the corgi
                 corgiScene.boxColor = StdDraw.RED;
                 if(health>0)
                     health--;
@@ -181,7 +174,7 @@ public  class corgiCore {
                 corgiScene.show(500);
                 s[i].reset();
             }
-            else{
+            else{ //neutral middle box
                 corgiScene.boxColor = StdDraw.CYAN;
             }
         }
